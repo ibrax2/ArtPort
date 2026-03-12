de5fb3e73516641875ec61b42a1858154abbc907
Ibrax2 <ibrahimameibrahim@gmail.com>
2026-03-12 17:44:52 -0400

    changed ownership of github so needed to update this

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://hanandin.github.io"],
+    origin: ["http://localhost:3000", "https://ibrax2.github.io"],
     credentials: true,
   }),
 );
